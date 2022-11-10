b6829ac9a0fdf8d76b58745c919b25a15f645ed8
ElDirolo <rosaldidac@gmail.com>
2022-11-10 12:36:46 +0100

    FUNCIONAAAAAAAAAAAAAA

--- cuidao/New Unity Project/Assets/scripst/LightingManager.cs
+++ cuidao/New Unity Project/Assets/scripst/LightingManager.cs
@@ -13,9 +13,31 @@ public class LightingManager : MonoBehaviour
     //Variables
     [SerializeField, Range(0, 24)] private float TimeOfDay;
 
+    private void Update()
+    {
+        if(Preset == null)
+            return;
+        if(Application.isPlaying)
+        {
+            TimeOfDay += Time.deltaTime;
+            TimeOfDay %= 24; //Clamp between 0-24 h
+            UpdateLighting(TimeOfDay / 24f);
+        }
+        else
+        {
+            UpdateLighting(TimeOfDay / 24f);
+        }
+    }
+
     private void UpdateLighting(float timePercent)
     {
-
+            RenderSettings.ambientLight = Preset.AmbientColor.Evaluate(timePercent);
+            RenderSettings.fogColor = Preset.FogColor.Evaluate(timePercent);
+            if(DirectionalLight != null)
+            {
+                DirectionalLight.color = Preset.DirectionalColor.Evaluate(timePercent);
+                DirectionalLight.transform.localRotation = Quaternion.Euler(new Vector3((timePercent * 360f) - 90f, 170, 0));
+            }
     }
 
     private void OnValidate()
@@ -28,7 +50,7 @@ public class LightingManager : MonoBehaviour
         }
         else
         {
-            Light[] light = GameObject.FindObjectsOfType<Light>();
+            Light[] lights = GameObject.FindObjectsOfType<Light>();
             foreach(Light light in lights)
             {
                 if(light.type == LightType.Directional)
